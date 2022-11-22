9318885909032788034fd285c94037e8a01ec4c2
Mike Kravtsov <mike@index.supply>
2022-11-22 13:01:33 -0800

    Updates

---
+++ precache-manifest.b3989564a30f93d49105e3d090539c07.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a820003980b70796db87303de2eac1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "38b35e9ded91167e9c03",
+    "url": "/portfolio/static/js/2.2f441404.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/portfolio/static/js/2.2f441404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d23f5ba4ba88b1c52fa",
+    "url": "/portfolio/static/js/main.6d22211c.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "727e7dad38ee2e1b30637cfbc9c300db",
+    "url": "/portfolio/static/media/bring.727e7dad.png"
+  },
+  {
+    "revision": "b12b487be4a73119b3e2bd74853e4ee2",
+    "url": "/portfolio/static/media/burger.b12b487b.png"
+  },
+  {
+    "revision": "7d8aea3851cbc1f4cfccfbac2b2a0476",
+    "url": "/portfolio/static/media/crystal_collector.7d8aea38.png"
+  },
+  {
+    "revision": "a0babcfc1ea1cab1a2a00234ea4fa0b7",
+    "url": "/portfolio/static/media/cu-users.a0babcfc.png"
+  },
+  {
+    "revision": "3fafc738623f3571a743dac1490ef81d",
+    "url": "/portfolio/static/media/friends_finder.3fafc738.png"
+  },
+  {
+    "revision": "339e84c708204edef84fdeee1d6a5d2c",
+    "url": "/portfolio/static/media/giphy.339e84c7.png"
+  },
+  {
+    "revision": "7acd6632aa5112ae28fe08c0d441702b",
+    "url": "/portfolio/static/media/lion.7acd6632.png"
+  },
+  {
+    "revision": "b657ed05a8eccea9be7c5df7e5e49608",
+    "url": "/portfolio/static/media/office_brunch.b657ed05.png"
+  },
+  {
+    "revision": "950df7deac388cbe187e14566c7600d1",
+    "url": "/portfolio/static/media/the_psychic_game.950df7de.png"
+  },
+  {
+    "revision": "1c3c0d6fc3a6b14c70b63b9547ec7ef6",
+    "url": "/portfolio/static/media/train_table.1c3c0d6f.png"
+  },
+  {
+    "revision": "73a91337749ca726778a4637bd89778a",
+    "url": "/portfolio/static/media/trivia_game.73a91337.png"
+  }
+]);
